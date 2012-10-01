bd6c1d488c51f563b8b7686f723dc028e3154fab
Luiz Branco <me@luizbranco.com>
2012-09-30 22:25:22 -0300

    Update grunt file to gruntfile template

--- grunt.js
+++ grunt.js
@@ -1,17 +1,37 @@
+/*global module:false*/
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
-    test: {
-      files: ['test/**/*.js']
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
     lint: {
       files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
     },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
     watch: {
       files: '<config:lint.files>',
-      tasks: 'default'
+      tasks: 'lint qunit'
     },
     jshint: {
       options: {
@@ -25,15 +45,14 @@ module.exports = function(grunt) {
         undef: true,
         boss: true,
         eqnull: true,
-        node: true
+        browser: true
       },
-      globals: {
-        exports: true
-      }
-    }
+      globals: {}
+    },
+    uglify: {}
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', 'lint qunit concat min');
 
 };
