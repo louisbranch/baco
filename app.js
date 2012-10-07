2effddd306df5f490fd377ad48b0b8ce979a2428
Luiz Branco <me@luizbranco.com>
2012-10-07 19:24:59 -0300

    Remove use routes

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -27,8 +22,5 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'));
