0c3bf232283407f69618e41a11ba8afa8155cdcc
Luiz Branco <me@luizbranco.com>
2012-10-20 21:55:46 -0300

    Move from jade to handlebars

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var hbs = require('hbs');
+var fs = require('fs');
+
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -20,6 +23,14 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
+
+function loadPartial(name) {
+  var file = fs.readFileSync(__dirname + '/views/_'+name+'.hbs', 'utf8');
+  hbs.registerPartial(name, file);
+}
+
+loadPartial('header');
+loadPartial('menu');
 
 app.get('/', routes.index);
 
