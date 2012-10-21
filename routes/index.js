8e393a77a656578c82bd371cf91a4274ad3f0f51
Luiz Branco <me@luizbranco.com>
2012-10-21 01:54:37 -0200

    Add menu

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var stories = require('../lib/stories');
 
+var favorites = [
+  {name: 'Escrever Conto', icon: 'pencil'},
+  {name: 'Mais Recentes', icon: 'inbox'},
+  {name: 'Mais Lidos', icon: 'eye-open'},
+  {name: 'Mais Votados', icon: 'star'},
+  {name: 'Conto Aleatorio', icon: 'random'}
+];
+
 exports.index = function(req, res){
   stories.summary(function(err, body) {
     if (!err) {
-      res.render('index', { stories: body.rows });
+      res.render('index', { stories: body.rows, favorites: favorites });
     }
   });
 };
